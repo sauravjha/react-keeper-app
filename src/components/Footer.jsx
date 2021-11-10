0b552b8944037c98212420d38eb47b6e8ed06117
Saurav Jha <sauravjha24@gmail.com>
2021-11-10 16:07:57 +1100

    Keeper set up part 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+var date = new Date();
+var year = date.getFullYear();
+
+function Footer() {
+    return <footer><p>Copyright ⓒ {year}</p></footer>
+}
+
+export default Footer;
